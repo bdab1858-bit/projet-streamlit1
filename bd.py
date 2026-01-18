648a7ef1704bb04905fc4244d3db7a746087fe42
bdab1858-bit <bdab1858@gmail.com>
2026-01-18 01:17:45 +0100

    Update bd.py

--- bd.py
+++ bd.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="dpg-d5lsqtumcj7s73bm6k1g-a",
+        host="dpg-d5lsqtumcj7s73bm6k1g-a.oregon-postgres.render.com",
         database="edt_universitaire",
         user="admin",
         password="8ne6EUYzDjKXxv3d9axZElqbrV7w0skv",
