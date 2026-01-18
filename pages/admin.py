1d0938c93b8a5c4e6699b8c7522c18fa57925ee4
bdab1858-bit <bdab1858@gmail.com>
2026-01-18 15:58:29 +0100

    Fix typo in sys.path append for file reference

--- pages/admin.py
+++ pages/admin.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 try:
     from generate_edt import generate_exam_schedule, persist_schedule_to_db
